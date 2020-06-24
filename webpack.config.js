4ff3dcf977be11ce2244c88a36a1a23193509c4e
Edwin Sanchez <edwin.s.g@hotmail.com>
2020-06-23 20:47:10 -0500

    fake api

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,17 @@ module.exports = {
                   'css-loader',
                   'sass-loader'
                 ],
+              },
+              {
+                  test: /\.(png|jpg)$/,
+                  use:[
+                      {
+                          'loader': 'file-loader',
+                          options:{
+                              name:'assets/[hash].[ext]'
+                            }
+                      }
+                  ]
               }
         ]
     },
